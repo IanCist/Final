c35c3946aa7903054f9698cbc6e7a880d15b2c5d
IanCist <iantcisternas@gmail.com>
2024-05-24 13:44:44 -0400

    added another product page and google maps

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
 
 // Automatic Slideshow - change image every 4 seconds
 var myIndex = 0;
-carousel();
 
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 4000);    
-}
+
 
 // Used to toggle the menu on small screens when clicking on the menu button
 function myFunction() {
@@ -46,3 +35,15 @@ $(document).ready(function() {
   });
  
 });
+let map;
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+initMap();
